1d0d9befdc79079f61e35f28e63d6290a3abc922
davidpaz06 <davidalbertopaz@gmail.com>
2025-06-19 17:24:35 -0400

    first commit

---
+++ app/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-blue-400">
+      <h1 className=" text-amber-300"> Header One</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.ts
@@ -0,0 +1,3 @@
+export default {
+  content: ["./index.html", "./app/**/*.{js,ts,jsx,tsx}"],
+};
