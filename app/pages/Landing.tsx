e511e1d460b911c7c6ba47f466938a24d5e95bdb
davidpaz06 <davidalbertopaz@gmail.com>
2025-06-29 16:47:24 -0400

    feat(Landing): restructure landing page layout and update navigation links

--- app/pages/Landing.tsx
+++ app/pages/Landing.tsx
@@ -1,12 +1,53 @@
+import { Link } from "react-router-dom";
+
 const Landing = () => (
-  <div className="flex flex-col items-center justify-center min-h-screen bg-blue-400">
-    <h1 className="text-4xl font-bold text-amber-300 mb-4">Bienvenido a Campus Academy</h1>
-    <p className="text-lg text-white mb-6">Tu plataforma de aprendizaje</p>
-    <div className="flex gap-4">
-      <a href="/login" className="bg-white text-blue-600 px-4 py-2 rounded shadow hover:bg-blue-100">Login</a>
-      <a href="/register" className="bg-white text-blue-600 px-4 py-2 rounded shadow hover:bg-blue-100">Register</a>
-    </div>
-  </div>
+  <main>
+    <section>
+      <div className="flex flex-col items-center justify-center min-h-[90vh] bg-blue-400">
+        <h1 className="text-4xl font-bold text-amber-300 mb-4">
+          Bienvenido a Campus Academy
+        </h1>
+        <p className="text-lg text-white mb-6">Tu plataforma de aprendizaje</p>
+        <div className="flex gap-4 border-2 border-blue-600 rounded-lg">
+          <Link
+            to="/signin"
+            className="bg-white text-blue-600 px-4 py-2 rounded shadow hover:bg-blue-100"
+          >
+            Sign In
+          </Link>
+          <Link
+            to="/signup"
+            className="bg-white text-blue-600 px-4 py-2 rounded shadow hover:bg-blue-100"
+          >
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div className="flex flex-col items-center justify-center min-h-[100vh] bg-red-400">
+        <h1 className="text-4xl font-bold text-amber-300 mb-4">
+          Bienvenido a Campus Academy
+        </h1>
+        <p className="text-lg text-white mb-6">Tu plataforma de aprendizaje</p>
+        <div className="flex gap-4">
+          <Link
+            to="/signin"
+            className="bg-white text-blue-600 px-4 py-2 rounded shadow hover:bg-blue-100"
+          >
+            signin
+          </Link>
+          <Link
+            to="/signup"
+            className="bg-white text-blue-600 px-4 py-2 rounded shadow hover:bg-blue-100"
+          >
+            signup
+          </Link>
+        </div>
+      </div>
+    </section>
+  </main>
 );
 
 export default Landing;
