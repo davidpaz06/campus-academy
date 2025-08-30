fe21d4cdc006ca4159ec98d53244b04612b7f628
stroconisf <stroconisf@gmail.com>
2025-08-29 20:43:03 -0400

    feat(CreateCourse): enhance course creation navigation and layout; add dynamic module handling and styling adjustments

--- app/pages/private/academy/CreateCourse.tsx
+++ app/pages/private/academy/CreateCourse.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Icon } from "@iconify/react";
 //Interfaces
 import type {
   Course,
@@ -10,7 +11,7 @@ import "./CreateCourse.css";
 //Pages
 import CreateCourseInfo from "./CreateCourseInfo";
 
-const InitialStep = 1;
+const InitialStep = 2;
 
 const mockCourse: Course = {
   id: "1",
@@ -35,90 +36,35 @@ const mockCourse: Course = {
           type: "Video",
           file: "This module dives deep into advanced JavaScript concepts.",
         },
+      ],
+    },
+    {
+      title: "Getting Started with React",
+      lessons: [
         {
-          title: "JavaScript Basics",
+          title: "React Basics",
           type: "Reading",
-          file: "This module covers the basics of JavaScript.",
+          file: "This module covers the basics of React.",
         },
         {
-          title: "JavaScript Test",
-          type: "Test",
-          file: "<test_file_path>",
+          title: "React Advanced",
+          type: "Video",
+          file: "This module dives deep into advanced React concepts.",
         },
       ],
     },
     {
-      title: "Introduction to JavaScript",
+      title: "Node.js and Express",
       lessons: [
         {
-          title: "JavaScript Basics",
+          title: "Node.js Basics",
           type: "Reading",
-          file: "This module covers the basics of JavaScript.",
+          file: "This module covers the basics of Node.js.",
         },
         {
-          title: "JavaScript Advanced",
-          type: "Reading",
-          file: "This module dives deep into advanced JavaScript concepts.",
-        },
-      ],
-    },
-    {
-      title: "Introduction to JavaScript",
-      lessons: [
-        {
-          title: "JavaScript Basics",
-          type: "Reading",
-          file: "This module covers the basics of JavaScript.",
-        },
-        {
-          title: "JavaScript Advanced",
-          type: "Reading",
-          file: "This module dives deep into advanced JavaScript concepts.",
-        },
-      ],
-    },
-    {
-      title: "Introduction to JavaScript",
-      lessons: [
-        {
-          title: "JavaScript Basics",
-          type: "Reading",
-          file: "This module covers the basics of JavaScript.",
-        },
-        {
-          title: "JavaScript Advanced",
-          type: "Reading",
-          file: "This module dives deep into advanced JavaScript concepts.",
-        },
-      ],
-    },
-    {
-      title: "Advanced React",
-      lessons: [
-        {
-          title: "React Hooks",
-          type: "Reading",
-          file: "This module covers the basics of React Hooks.",
-        },
-        {
-          title: "State Management",
-          type: "Reading",
-          file: "This module dives deep into state management in React.",
-        },
-      ],
-    },
-    {
-      title: "Advanced React",
-      lessons: [
-        {
-          title: "React Hooks",
-          type: "Reading",
-          file: "This module covers the basics of React Hooks.",
-        },
-        {
-          title: "State Management",
-          type: "Reading",
-          file: "This module dives deep into state management in React.",
+          title: "Node.js Advanced",
+          type: "Video",
+          file: "This module dives deep into advanced Node.js concepts.",
         },
       ],
     },
@@ -126,8 +72,15 @@ const mockCourse: Course = {
 };
 
 export default function CreateCourse() {
+  const [step, setStep] = useState(InitialStep);
+  const handleStepChange = (action: string) => {
+    action == "next" ? setStep(step + 1) : setStep(step - 1);
+    console.log(course);
+  };
+
+  const [activeModule, setActiveModule] = useState<number>(0);
   const [course, setCourse] = useState<Course>(
-    //   {
+    // {
     //   id: "",
     //   info: {
     //     name: "",
@@ -137,14 +90,64 @@ export default function CreateCourse() {
     //     skills: [],
     //   },
     //   modules: [],
-    // }
+    // },
     mockCourse
   );
-  const [step, setStep] = useState(InitialStep);
 
-  const handleStepChange = (action: string) => {
-    action == "next" ? setStep(step + 1) : setStep(step - 1);
-    console.log(course);
+  const handleNavActions = (action: string) => {
+    switch (action) {
+      case "prev":
+        // Navigate to the previous module
+        if (activeModule > 0) {
+          setActiveModule(activeModule - 1);
+        }
+        break;
+
+      case "next":
+        // Navigate to the next module
+        if (activeModule < course.modules.length - 1) {
+          setActiveModule(activeModule + 1);
+        }
+
+        if (activeModule === course.modules.length - 1) {
+          //add new module
+          const newModule: CourseModule = {
+            title: "Module " + (course.modules.length + 1),
+            lessons: [],
+          };
+          setCourse({ ...course, modules: [...course.modules, newModule] });
+          setActiveModule(course.modules.length);
+          break;
+        }
+    }
+  };
+
+  const handleNavClass = (activeModule: number, modulesLength: number) => {
+    modulesLength = modulesLength - 1;
+
+    if (activeModule === 0 && modulesLength === 1) {
+      return "initial";
+    } else if (activeModule === 0 && activeModule < modulesLength) {
+      return "first";
+    } else if (activeModule > 0 && activeModule < modulesLength) {
+      return "middle";
+    } else if (activeModule === modulesLength) {
+      return "last";
+    }
+  };
+
+  const handleModuleClass = (idx: number) => {
+    if (idx < activeModule) {
+      return "prev-module";
+    } else if (idx === activeModule) {
+      return "present-module";
+    } else if (idx > activeModule) {
+      return "next-module";
+    }
+  };
+
+  const handleActiveModule = (index: number) => {
+    setActiveModule(index);
   };
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -211,18 +214,24 @@ export default function CreateCourse() {
             <label className="course-name">
               <h2>{course.info.name}</h2>
               <section className="course-extras">
-                <label>
-                  <strong>About</strong>
-                  <p>{course.info.about}</p>
-                </label>
-                <label>
-                  <strong>Instructor</strong>
-                  <p>{course.info.instructor}</p>
-                </label>
-                <label>
-                  <strong>Skills</strong>
-                  <p>{course.info.skills.join(", ")}</p>
-                </label>
+                {course.info.about ? (
+                  <label>
+                    <strong>About</strong>
+                    <p>{course.info.about}</p>
+                  </label>
+                ) : null}
+                {course.info.instructor ? (
+                  <label>
+                    <strong>Instructor</strong>
+                    <p>{course.info.instructor}</p>
+                  </label>
+                ) : null}
+                {course.info.skills.length > 0 ? (
+                  <label>
+                    <strong>Skills</strong>
+                    <p>{course.info.skills.join(", ")}</p>
+                  </label>
+                ) : null}
               </section>
             </label>
             <div className="module-list">
@@ -241,7 +250,7 @@ export default function CreateCourse() {
                         )
                       )}
                     </ul>
-                    <h3 className="module-title-l">Module {moduleIndex + 1}</h3>
+                    <p className="module-number">Module {moduleIndex + 1}</p>
                   </div>
                 )
               )}
@@ -262,7 +271,75 @@ export default function CreateCourse() {
               </button>
             </footer>
           </aside>
-          <form className="create-course-form"></form>
+          {/* <button style={{ display: "none" }}>
+            <Icon icon="material-symbols:book-ribbon-rounded" />
+            <Icon icon="material-symbols:smart-display-rounded" />
+            <Icon icon="material-symbols:unknown-document-rounded" />
+          </button> */}
+          <form
+            className="create-course-form"
+            onSubmit={handleSubmit}
+            onKeyDown={handleKeyDown}
+          >
+            <section
+              className={`create-course-nav ${handleNavClass(
+                activeModule,
+                course.modules.length
+              )}`}
+            >
+              <div
+                className="nav-button prev-module-button"
+                onClick={() => handleNavActions("prev")}
+              >
+                <Icon
+                  className="nav-icon prev-icon"
+                  icon="material-symbols:arrow-back-rounded"
+                />
+              </div>
+              <div className="create-course-active-module">
+                <input
+                  type="text"
+                  value={course.modules[activeModule].title}
+                  onSubmit={(e) => e.preventDefault()}
+                  onChange={(e) => {
+                    const updatedModules = [...course.modules];
+                    updatedModules[activeModule] = {
+                      ...updatedModules[activeModule],
+                      title: e.target.value,
+                    };
+                    setCourse({ ...course, modules: updatedModules });
+                  }}
+                  className="active-module-input"
+                />
+              </div>
+              <div
+                className="nav-button next-module-button"
+                onClick={() => handleNavActions("next")}
+              >
+                <Icon
+                  className="nav-icon next-icon"
+                  icon="material-symbols:arrow-forward-rounded"
+                />
+                <Icon
+                  className="nav-icon add-icon"
+                  icon="material-symbols:add-rounded"
+                />
+              </div>
+            </section>
+            <section className="create-course-content">
+              {course.modules.map((module, idx) => {
+                return (
+                  <div
+                    key={idx}
+                    className={`module-box ${handleModuleClass(idx)}`}
+                    onClick={() => handleActiveModule(idx)}
+                  >
+                    Module {idx + 1} - {module.title}
+                  </div>
+                );
+              })}
+            </section>
+          </form>
         </div>
       );
   }
